27f4e187c064bd62e995dfe32777701a4e2a5756
Lloyd Wallis <lloyd.wallis@bbc.co.uk>
2017-01-11 12:16:28 +0000

    Fix vod.mp4, add move tfxd into the traf, and remove the tfra from the mfra.

--- src/main/java/baninvented/EmsgFragmentedMp4Builder.java
+++ src/main/java/baninvented/EmsgFragmentedMp4Builder.java
@@ -4,7 +4,15 @@ import com.googlecode.mp4parser.authoring.builder.FragmentedMp4Builder;
 import com.coremedia.iso.boxes.*;
 import com.coremedia.iso.boxes.fragment.*;
 import com.googlecode.mp4parser.boxes.piff.*;
+import com.googlecode.mp4parser.boxes.mp4.samplegrouping.GroupEntry;
+import com.googlecode.mp4parser.boxes.mp4.samplegrouping.SampleToGroupBox;
+import com.googlecode.mp4parser.boxes.mp4.samplegrouping.SampleGroupDescriptionBox;
+import com.googlecode.mp4parser.authoring.tracks.CencEncryptedTrack;
 import com.googlecode.mp4parser.authoring.*;
+
+import java.util.*;
+
+import static com.googlecode.mp4parser.util.CastUtils.l2i;
 
 public class EmsgFragmentedMp4Builder extends FragmentedMp4Builder {
 	private long offset = 0L;
@@ -25,7 +33,66 @@ public class EmsgFragmentedMp4Builder extends FragmentedMp4Builder {
         parent.addBox(tfdt);
     }
 
-    protected void createTfxd(long startSample, Track track, MovieFragmentBox parent) {
+    // The original version doesn't add a tfxd
+    @Override
+    protected void createTraf(long startSample, long endSample, Track track, int sequenceNumber, MovieFragmentBox parent) {
+        TrackFragmentBox traf = new TrackFragmentBox();
+        parent.addBox(traf);
+        createTfhd(startSample, endSample, track, sequenceNumber, traf);
+        createTfdt(startSample, track, traf);
+        createTrun(startSample, endSample, track, sequenceNumber, traf);
+
+        if (track instanceof CencEncryptedTrack) {
+            createSaiz(startSample, endSample, (CencEncryptedTrack) track, sequenceNumber, traf);
+            createSenc(startSample, endSample, (CencEncryptedTrack) track, sequenceNumber, traf);
+            createSaio(startSample, endSample, (CencEncryptedTrack) track, sequenceNumber, traf);
+        }
+
+
+        Map<String, List<GroupEntry>> groupEntryFamilies = new HashMap<String, List<GroupEntry>>();
+        for (Map.Entry<GroupEntry, long[]> sg : track.getSampleGroups().entrySet()) {
+            String type = sg.getKey().getType();
+            List<GroupEntry> groupEntries = groupEntryFamilies.get(type);
+            if (groupEntries == null) {
+                groupEntries = new ArrayList<GroupEntry>();
+                groupEntryFamilies.put(type, groupEntries);
+            }
+            groupEntries.add(sg.getKey());
+        }
+
+
+        for (Map.Entry<String, List<GroupEntry>> sg : groupEntryFamilies.entrySet()) {
+            SampleGroupDescriptionBox sgpd = new SampleGroupDescriptionBox();
+            String type = sg.getKey();
+            sgpd.setGroupEntries(sg.getValue());
+            sgpd.setGroupingType(type);
+            SampleToGroupBox sbgp = new SampleToGroupBox();
+            sbgp.setGroupingType(type);
+            SampleToGroupBox.Entry last = null;
+            for (int i = l2i(startSample - 1); i < l2i(endSample - 1); i++) {
+                int index = 0;
+                for (int j = 0; j < sg.getValue().size(); j++) {
+                    GroupEntry groupEntry = sg.getValue().get(j);
+                    long[] sampleNums = track.getSampleGroups().get(groupEntry);
+                    if (Arrays.binarySearch(sampleNums, i) >= 0) {
+                        index = j + 0x10001;
+                    }
+                }
+                if (last == null || last.getGroupDescriptionIndex() != index) {
+                    last = new SampleToGroupBox.Entry(1, index);
+                    sbgp.getEntries().add(last);
+                } else {
+                    last.setSampleCount(last.getSampleCount() + 1);
+                }
+            }
+            traf.addBox(sgpd);
+            traf.addBox(sbgp);
+        }
+
+        createTfxd(startSample, track, traf);
+    }
+
+    protected void createTfxd(long startSample, Track track, TrackFragmentBox parent) {
         TfxdBox tfxd = new TfxdBox();
         tfxd.fragmentAbsoluteTime = getBaseMediaDecodeTime(startSample, track);
         tfxd.fragmentAbsoluteDuration = getSampleDuration(startSample, track);
@@ -36,13 +103,22 @@ public class EmsgFragmentedMp4Builder extends FragmentedMp4Builder {
         MovieFragmentBox moof = new MovieFragmentBox();
         createMfhd(startSample, endSample, track, sequenceNumber, moof);
         createTraf(startSample, endSample, track, sequenceNumber, moof);
-        createTfxd(startSample, track, moof);
 
         TrackRunBox firstTrun = moof.getTrackRunBoxes().get(0);
         firstTrun.setDataOffset(1); // dummy to make size correct
         firstTrun.setDataOffset((int) (8 + moof.getSize())); // mdat header + moof size
 
         return moof;
+    }
+
+    // CodeShop require no Tfra boxes in the Mfra
+    @Override
+    protected Box createMfra(Movie movie, Container isoFile) {
+        MovieFragmentRandomAccessBox mfra = new MovieFragmentRandomAccessBox();
+        MovieFragmentRandomAccessOffsetBox mfro = new MovieFragmentRandomAccessOffsetBox();
+        mfra.addBox(mfro);
+        mfro.setMfraSize(mfra.getSize());
+        return mfra;
     }
 
     protected long getBaseMediaDecodeTime(long startSample, Track track) {
